8a627983dff914df830b7cdafe6f39ebba948f5a
Maximilian Liebmann <lima@noreply.git.dominikstahl.dev>
2025-04-19 22:14:56 +0200

    fix: renamed button related files

---
+++ src/components/button.tsx
@@ -0,0 +1,39 @@
+import style from './button.module.css';
+
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function Button({
+  type,
+  children,
+  mode = 'primary',
+  icon,
+}: {
+  type?: 'button' | 'submit' | 'reset';
+  children?: React.ReactNode;
+  mode?: 'primary' | 'warning' | 'success' | 'danger';
+  icon?: IconProp;
+}) {
+  if (!icon) {
+    return (
+      <button
+        className={style.button + ' ' + style['style_' + mode]}
+        type={type}
+      >
+        <span>{children}</span>
+      </button>
+    );
+  } else {
+    return (
+      <button
+        className={
+          style.button + ' ' + style['style_' + mode] + ' ' + style['icon']
+        }
+        type={type}
+      >
+        <FontAwesomeIcon icon={icon} height={25} />
+        <span>{children}</span>
+      </button>
+    );
+  }
+}
