d781567b4feaafb407df534fe519dbd56b1d88d8
Maximilian Liebmann <lima@noreply.git.dominikstahl.dev>
2025-04-19 22:20:46 +0200

    feat: added logout route and icon

--- src/components/user/logout-button.tsx
+++ src/components/user/logout-button.tsx
@@ -1,15 +1,18 @@
 import { signOut } from '@/auth';
-import Button from '../Button';
+import Button from '../button';
+import { faDoorOpen } from '@fortawesome/free-solid-svg-icons';
 
 export function Logout() {
   return (
     <form
       action={async () => {
         'use server';
-        await signOut();
+        await signOut({ redirectTo: '/login' });
       }}
     >
-      <Button type='submit'>Sign Out</Button>
+      <Button type='submit' mode='primary' icon={faDoorOpen}>
+        Sign Out
+      </Button>
     </form>
   );
 }
