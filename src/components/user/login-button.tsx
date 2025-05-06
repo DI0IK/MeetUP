d7e682d4843419486de12cf4d4bcc663378d1cfc
SomeCodecat <88855796+SomeCodecat@users.noreply.github.com>
2025-05-06 13:49:29 +0200

    fix: removes unused props from login component

--- src/components/user/login-button.tsx
+++ src/components/user/login-button.tsx
@@ -1,6 +1,6 @@
 import Button from '../button';
 
-export default function Login({}: {}) {
+export default function Login() {
   return (
     <form>
       <Button type='submit' mode='neutral' width={250}>
