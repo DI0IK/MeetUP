0187b2e955432af2972ccf24eb0dde808f87d711
SomeCodecat <88855796+SomeCodecat@users.noreply.github.com>
2025-05-08 13:04:16 +0200

    fix: update middleware to check MEETUP_SKIP_LOGIN environment variable before redirecting to login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
 import { auth } from '@/auth';
 
 export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname !== '/login') {
+  if (
+    !req.auth &&
+    req.nextUrl.pathname !== '/login' &&
+    process.env.MEETUP_SKIP_LOGIN === 'true'
+  ) {
     const newUrl = new URL('/login', req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
