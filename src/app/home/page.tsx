b68a5618867b7f8d58cc278e0ffe1945275c580c
Dominik Stahl <dominik@samdj.de>
2025-05-12 21:24:23 +0200

    feat: add logout page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,3 @@
-import { Logout } from '@/components/user/sso-logout-button';
 import { RedirectButton } from '@/components/user/redirect-button';
 import { ThemePicker } from '@/components/user/theme-picker';
 
@@ -8,7 +7,7 @@ export default function Home() {
       <div className='absolute top-4 right-4'>{<ThemePicker />}</div>
       <div>
         <h1>Home</h1>
-        <Logout />
+        <RedirectButton redirectUrl='/logout' buttonText='Logout' />
         <RedirectButton redirectUrl='/settings' buttonText='Settings' />
       </div>
     </div>
