e5359ea4d3b1e5a182075e22834756b0741e56e3
Micha <michabokelmann@web.de>
2025-05-22 13:18:33 +0200

    feat: add favicon and web app manifest files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
+      <head>
+        <link
+          rel='icon'
+          type='image/png'
+          href='/favicon-96x96.png'
+          sizes='96x96'
+        />
+        <link rel='icon' type='image/svg+xml' href='/favicon-dark.svg' media='(prefers-color-scheme: dark)'/>
+        <link rel='icon' type='image/svg+xml' href='/favicon-light.svg' media='(prefers-color-scheme: light)'/>
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+      </head>
       <body>
         <ThemeProvider
           attribute='class'
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { auth as middleware } from '@/auth';
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|favicon-96x96.png|favicon.svg|site.webmanifest|web-app-manifest-192x192.png|web-app-manifest-512x512.png|favicon-dark.svg|favicon-light.svg).*)'],
 };
