173be63a9c9f5b7f3e8d3342711eb9d83b275e75
Micha <michabokelmann@web.de>
2025-06-25 13:11:14 +0200

    feat(events): add event edit page

---
+++ src/app/events/edit/[eventID]/page.tsx
@@ -0,0 +1,26 @@
+import { ThemePicker } from '@/components/misc/theme-picker';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import EventForm from '@/components/forms/event-form';
+import { Suspense } from 'react';
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ eventID: string }>;
+}) {
+  const eventID = (await params).eventID;
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <div className='absolute top-4 right-4'>{<ThemePicker />}</div>
+      <Card className='w-[80%] max-w-screen p-0 gap-0 max-xl:w-[95%] max-h-[90vh] overflow-auto'>
+        <CardHeader className='p-0 m-0 gap-0' />
+
+        <CardContent>
+          <Suspense>
+            <EventForm type='edit' eventId={eventID} />
+          </Suspense>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
