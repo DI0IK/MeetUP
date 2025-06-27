92be324186ebdb2cee0a2892d12e59c92853f71e
Micha <michabokelmann@web.de>
2025-06-27 10:36:40 +0200

    style: update home page

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -4,10 +4,21 @@ import Calendar from '@/components/calendar';
 import { useGetApiUserMe } from '@/generated/api/user/user';
 
 export default function Home() {
-  const { data } = useGetApiUserMe();
+  const { data, isLoading } = useGetApiUserMe();
 
   return (
-    <div className='max-h-full flex justify-center items-center'>
+    <div className='grid grid-cols-1 w-full place-items-center'>
+      <div className='w-full sm:w-[90%] mb-4'>
+        <h1 className='text-2xl font-bold text-center'>
+          Welcome, <wbr />
+          <span style={{ whiteSpace: 'nowrap' }}>
+            {isLoading
+              ? 'Loading...'
+              : data?.data.user?.first_name || 'Unknown User'}{' '}
+            &#128075;
+          </span>
+        </h1>
+      </div>
       <div className='w-full sm:w-[90%]'>
         <Calendar
           userId={data?.data.user?.id}
